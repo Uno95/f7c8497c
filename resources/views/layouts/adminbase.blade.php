07c0e1f85e10572827d1bd2e0d79738217b478c1
Uno95 <131113140@students.mikroskil.ac.id>
2018-04-11 10:38:25 +0700

    Adding Styling and nav resource files

---
+++ resources/views/layouts/adminbase.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Bootstrap Core Css -->
+    <link href="{{ asset('/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <title>PSPI WASTE APP</title>
+</head>
+<body>
+    @include('admin.navigation')
+    
+    <!-- Jquery Core Js -->
+    <script src="{{ asset('/bootstrap/js/jquery-2.0.2.min.js') }}"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="{{ asset('/bootstrap/js/bootstrap.js') }}"></script>
+</body>
+</html>
